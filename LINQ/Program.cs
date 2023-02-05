96238cc4b133f53a485e32232c71e73e2adfd192
Katelyn-Collier <katelyncollier14@gmail.com>
2023-02-05 15:18:52 -0600

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var games = new List<string>() { "The Elder Scrolls V: Skyrim", "League of Legends", "Dead by Daylight", "Civilization VI", "Lost Ark" };
+
+            games = games.OrderBy(g => g.Length).ToList();
+
+            for(int i = 0; i < games.Count; i++)
+            {
+                Console.WriteLine($"{games[i]}");
+            }
         }
     }
 }
